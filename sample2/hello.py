6997f52019216091ebde42b89a66282433ef5fba
teru0406alone <103554003+teru0406alone@users.noreply.github.com>
2022-05-25 14:11:37 +0900

    Update hello.py

--- sample2/hello.py
+++ sample2/hello.py
@@ -1,10 +1,9 @@
+
 from flask import flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
-
-    if __name__ == '__main__':
-        app.run()
+    return render_template('Hello, World!')
